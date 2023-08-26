4dd61f911561dc310c1a5e67dffd6ac0dbd69346
Fernando Herrera <herrera.fernando1209@gmail.com>
2023-08-25 23:49:19 -0300

    create beat done!

--- src/controllers/products/storeProduct.js
+++ src/controllers/products/storeProduct.js
@@ -1,9 +1,27 @@
 const path = require('path');
 const productsFilePath = path.join(__dirname, '../../data/products.json');
-const { leerJson } = require('../../data/index');
+const { leerJson, escribirJson } = require('../../data/index');
 
 module.exports = (req, res) => {
-    const products = leerJson();
-    console.log(req.file)
+    const products = leerJson(productsFilePath);
+    const obj = req.files.image;
+    //console.log(req.body)
+    // console.log(req.files)
+    // console.log(req.fileValidation)
+    if (!req.files.beat) {
+        return res.redirect('/products/add');
+    }
+    const newProduct = {
+        productId: products[products.length - 1].productId + 1,
+        name: req.body.title,
+        userId: 1,
+        category: req.body.category,
+        description: req.body.description,
+        image: req.files.image ? req.files["image"][0].filename : null,
+        beat: req.files["beat"][0].filename,
+        price: req.body.price
+    }
+    products.push(newProduct);
+    escribirJson(productsFilePath, products);
     return res.redirect('/');
 }
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const productsController = require('../controllers/productsController');
 const router = express.Router();
-const uploadImage = require('../middlewares/uploadImage')
-const uploadBeat= require('../middlewares/uploadBeat')
+const uploadBeat = require('../middlewares/uploadBeat')
+
+
 /* /products */
 router.get('/', productsController.index);
 router.get('/detail/:id', productsController.detail);
 router.get('/add', productsController.addProduct);
-router.post('/add',uploadImage.single('image'), productsController.storeProduct);
+router.post('/add', uploadBeat.fields([{ name: "image", maxCount: 1 }, { name: "beat", maxCount: 1 }]), productsController.storeProduct);
 module.exports = router;
 
-// ,uploadBeat.single('beat')
+// ,uploadImage.single('image')

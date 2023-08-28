8b53efa862df62c6d81bdac5b4a2915bf55e5371
Fernando Herrera <herrera.fernando1209@gmail.com>
2023-08-28 20:46:45 -0300

    fix cards index

--- public/js/script.js
+++ public/js/script.js
@@ -25,7 +25,7 @@
 //   enabled: true,
 //   nextEl: ".swiper-button-next",
 //       prevEl: ".swiper-button-prev",
-      
+
 //   },    
 //    pagination: {                     
 //         el: '.swiper-pagination',
@@ -73,11 +73,11 @@
 
 // function hasScrolled() {
 //     var st = $(this).scrollTop();
-    
+
 //     // Make sure they scroll more than delta
 //     if(Math.abs(lastScrollTop - st) <= delta)
 //         return;
-    
+
 //     // If they scrolled down and are past the navbar, add class .nav-up.
 //     // This is necessary so you never see what is "behind" the navbar.
 //     if (st > lastScrollTop && st > navbarHeight){
@@ -89,51 +89,47 @@
 //             $('footer').removeClass('nav-up').addClass('nav-down');
 //         }
 //     }
-    
+
 //     lastScrollTop = st;
 // }
 
 
-  // Cards
-  $(document).ready(function(){
-    var zindex = 10;
-    
-    $("div.card").click(function(e){
-      e.preventDefault();
-  
-      var isShowing = false;
-  
-      if ($(this).hasClass("show")) {
-        isShowing = true
-      }
-  
-      if ($("div.cards").hasClass("showing")) {
-        // a card is already in view
-        $("div.card.show")
-          .removeClass("show");
-  
-        if (isShowing) {
-          // this card was showing - reset the grid
-          $("div.cards")
-            .removeClass("showing");
-        } else {
-          // this card isn't showing - get in with it
-          $(this)
-            .css({zIndex: zindex})
-            .addClass("show");
-  
-        }
+// Cards
+$(document).ready(function () {
+  var zindex = 10;
 
-        zindex++;
-      } else {
-        // no cards in view
+  $(".card-title").click(function (e) {
+    e.preventDefault();
+    console.log(e.currentTarget.id);
+    var isShowing = false;
+    if ($('.card_' + e.currentTarget.id).hasClass("show")) {
+      isShowing = true
+    }
+
+    if ($("div.cards").hasClass("showing")) {
+      // a card is already in view
+      $("div.card.show")
+        .removeClass("show");
+
+      if (isShowing) {
+        // this card was showing - reset the grid
         $("div.cards")
-          .addClass("showing");
-        $(this)
-          .css({zIndex:zindex})
+          .removeClass("showing");
+      } else {
+        // this card isn't showing - get in with it
+        $('.card_' + e.currentTarget.id)
+          .css({ zIndex: zindex })
           .addClass("show");
-  
-        zindex++;
       }
-    });
+      zindex++;
+    } else {
+      // no cards in view
+      $("div.cards")
+        .addClass("showing");
+      $('.card_' + e.currentTarget.id)
+        .css({ zIndex: zindex })
+        .addClass("show");
+      zindex++;
+    }
   });
+});

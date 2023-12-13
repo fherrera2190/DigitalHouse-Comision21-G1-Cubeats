f953a4c63e7a3defeef455ce63790073ce721213
Fernando Herrera <herrera.fernando1209@gmail.com>
2023-12-13 15:35:21 -0300

    fix search products

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -4,20 +4,20 @@ module.exports = {
   index: async (req, res) => {
     try {
       const products = await db.Beat.findAll({
-        include: ["category", "producer"]
+        include: ["category", "producer"],
       });
       //console.log(req.session.userLogged)
       if (req.session.userLogged) {
         let userAllLikes = await db.Like.findAll({
           where: {
-            userId: req.session.userLogged.userId
-          }
+            userId: req.session.userLogged.userId,
+          },
         });
         console.log(userAllLikes);
-        userAllLikes = userAllLikes.map(elemnt => elemnt.dataValues.beatId);
+        userAllLikes = userAllLikes.map((elemnt) => elemnt.dataValues.beatId);
         //userAllLikes.includes(1)
         const categories = await db.Category.findAll();
-        return res.render("products", { products, categories, userAllLikes});
+        return res.render("products", { products, categories, userAllLikes });
       }
       const categories = await db.Category.findAll();
       return res.render("products", { products, categories });
@@ -31,5 +31,5 @@ module.exports = {
   searchProduct: require("./products/search"),
   deleteProduct: require("./products/deleteProduct"),
   editProduct: require("./products/editProduct"),
-  update: require("./products/update")
+  update: require("./products/update"),
 };

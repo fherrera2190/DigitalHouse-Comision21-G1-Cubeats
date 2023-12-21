3ac520bb578b21a5df694e38b942f88fdbf0791a
lucas-pausin <lucaspausin@gmail.com>
2023-12-21 11:45:51 -0300

    detalles..

--- public/js/searchValidator.js
+++ public/js/searchValidator.js
@@ -4,11 +4,6 @@ document
 		const searchValue = document.getElementById("search").value.trim();
 
 		if (searchValue === "") {
-			document.getElementById("searchError").classList.remove("hidden");
-			document.getElementById("searchError").classList.add("flex");
 			event.preventDefault();
-		} else {
-			document.getElementById("searchError").classList.add("hidden");
-			document.getElementById("searchError").classList.remove("flex");
 		}
 	});

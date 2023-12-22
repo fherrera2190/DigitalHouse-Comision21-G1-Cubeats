1b682a64e7affdb5ec1875572c2c2f271d992f3d
lucas-pausin <lucaspausin@gmail.com>
2023-12-22 10:09:26 -0300

    Detalle del producto..

--- src/app.js
+++ src/app.js
@@ -14,11 +14,12 @@ const cookieCheck = require("./middlewares/cookieCheck");
 const app = express();
 const apiUsersRouter = require("./routes/user.api.routes");
 const apiProductsRoutes = require("./routes/products.api.routes");
-const apiCartsRoutes= require('./routes/cart.api.routes')
+const apiCartsRoutes = require("./routes/cart.api.routes");
+const cors = require("cors");
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
+app.use(cors());
 //Middleware globales
 app.use(logger("dev"));
 app.use(express.json());
@@ -27,11 +28,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(methodOverride("_method"));
 app.use(
-  session({
-    secret: "CuBeatsX100pre",
-    resave: true,
-    saveUninitialized: true
-  })
+	session({
+		secret: "CuBeatsX100pre",
+		resave: true,
+		saveUninitialized: true,
+	})
 );
 app.use(cookieCheck);
 app.use(userSessionCheck);
@@ -47,21 +48,20 @@ app.use("/api/users", apiUsersRouter);
 app.use("/api/products", apiProductsRoutes);
 app.use("/api/carts", apiCartsRoutes);
 
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+	// render the error page
+	res.status(err.status || 500);
+	res.render("error");
 });
 
 module.exports = app;

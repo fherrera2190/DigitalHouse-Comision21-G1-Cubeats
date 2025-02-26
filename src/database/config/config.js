e4cf0ae8465f3e54b8cbf0c6b6f45e0e909c93e6
Fernando Herrera <herrera.fernando1209@gmail.com>
2025-02-26 11:00:34 -0300

    migrate to db postgresql

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -7,7 +7,7 @@ module.exports = {
     database: process.env.DB_DATABASE_DEV,
     host: process.env.DB_HOST_DEV,
     port: process.env.DB_PORT_DEV,
-    dialect: "mysql"
+    dialect: process.env.DB_DIALECT,
   },
   test: {
     username: process.env.DB_USERNAME_TEST,
@@ -15,13 +15,19 @@ module.exports = {
     database: process.env.DB_DATABASE_TEST,
     host: process.env.DB_HOST_TEST,
     port: process.env.DB_PORT_TEST,
-    dialect: "mysql"
+    dialect: process.env.DB_DIALECT,
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql"
-  }
+    username: process.env.DB_USERNAME_PROD,
+    password: process.env.DB_PASSWORD_PROD,
+    database: process.env.DB_DATABASE_PROD,
+    host: process.env.DB_HOST_PROD,
+    port: process.env.DB_PORT_PROD,
+    dialect: process.env.DB_DIALECT,
+    dialectOptions: {
+      ssl: {
+        require: process.env.DB_SSL_PROD === "true" ? true : false,
+      },
+    },
+  },
 };
